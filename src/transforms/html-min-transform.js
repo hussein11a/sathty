bff637330fa5e465b9d6886cefeed9d6067f9c2d
hussein11a <husseinalnahar11@gmail.com>
2025-05-23 22:42:45 +0300

    Update html-min-transform.js

--- src/transforms/html-min-transform.js
+++ src/transforms/html-min-transform.js
@@ -1,9 +1,15 @@
 // src/transforms/html-min-transform.js
-const { minifyHtml } = require("../utils/minify.js"); // استيراد الوظيفة من ملف utils
+// هذا التحويل (transform) يستخدم لتصغير حجم ملفات HTML الناتجة
+// عن طريق استدعاء وظيفة minifyHtml من ملف utils/minify.js.
+// يساعد في تحسين سرعة تحميل الصفحة.
+
+const { minifyHtml } = require("../utils/minify.js"); // استيراد الوظيفة minifyHtml من ملف utils/minify.js
 
 module.exports = function(content, outputPath) {
+  // تطبيق التحويل فقط على ملفات HTML في مسار الإخراج
   if (outputPath && outputPath.endsWith(".html")) {
-    return minifyHtml(content); // استخدام الوظيفة المستوردة
+    return minifyHtml(content); // استخدام الوظيفة المستوردة لتصغير محتوى HTML
   }
+  // إذا لم يكن الملف HTML، يتم إرجاع المحتوى كما هو
   return content;
 };
