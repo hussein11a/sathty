52cff132a8f774afff5feb9fe73fa8ccf5d40592
hussein11a <husseinalnahar11@gmail.com>
2025-05-23 22:40:07 +0300

    Update html-min-transform.js

--- src/transforms/html-min-transform.js
+++ src/transforms/html-min-transform.js
@@ -1,21 +1,9 @@
-
 // src/transforms/html-min-transform.js
-// هذا التحويل (transform) يستخدم لتصغير حجم ملفات HTML الناتجة.
-// يساعد في تحسين سرعة تحميل الصفحة عن طريق إزالة المسافات البيضاء الزائدة والتعليقات.
-
-const htmlmin = require("html-minifier"); // تأكد من تثبيت هذه الحزمة: npm install html-minifier
+const { minifyHtml } = require("../utils/minify.js"); // استيراد الوظيفة من ملف utils
 
 module.exports = function(content, outputPath) {
-  // تطبيق التحويل فقط على ملفات HTML في الإخراج
   if (outputPath && outputPath.endsWith(".html")) {
-    const minified = htmlmin.minify(content, {
-      useShortDoctype: true,
-      removeComments: true,
-      collapseWhitespace: true,
-      minifyCSS: true, // تصغير CSS المضمن في HTML
-      minifyJS: true,  // تصغير JavaScript المضمن في HTML
-    });
-    return minified;
+    return minifyHtml(content); // استخدام الوظيفة المستوردة
   }
   return content;
 };
